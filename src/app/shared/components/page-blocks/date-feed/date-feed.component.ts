58829fed8a74f66f5ff4fdae6e0588b245080b07
kazacoder <avtk.tlt@gmail.com>
2025-04-22 14:09:48 +0300

    main page is in progress, date swiper styles and config set up. Date filter component added

--- src/app/shared/components/page-blocks/date-feed/date-feed.component.ts
+++ src/app/shared/components/page-blocks/date-feed/date-feed.component.ts
@@ -1,69 +1,89 @@
 import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {DateItemComponent} from '../../cards/date-item/date-item.component';
-import Swiper from 'swiper';
-// import 'swiper/css/navigation'
-// import 'swiper'
-import {NgForOf} from '@angular/common';
+import {SwiperContainer} from 'swiper/element/bundle';
+import 'swiper/css/navigation'
+import 'swiper'
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'date-feed',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [DateItemComponent, NgForOf],
+  imports: [DateItemComponent, NgForOf, NgClass],
   templateUrl: './date-feed.component.html',
   styleUrl: './date-feed.component.scss'
 })
 export class DateFeedComponent implements AfterViewInit {
-
-  dates: string[] = []
+  daysOffset = 30;
+  daysInDateFeed = 100 ;
+  dates: {date: string, month: string, class: string, weekDay: string}[] = [];
+  dateSwiper: SwiperContainer | null = null;
+  dateSwiperParams = {
+    slidesPerView: 26,
+    slidesPerGroup: 26,
+    spaceBetween: 0,
+    allowTouchMove: false,
+    initialSlide: this.daysOffset,
+    navigation: {
+      nextEl: '.date-feed .swiper-button-next',
+      prevEl: '.date-feed .swiper-button-prev',
+    },
+    breakpoints: {
+      100: {
+        slidesPerView: "auto",
+        slidesPerGroup: 1,
+        allowTouchMove: true,
+      },
+      992: {
+        slidesPerView: 27,
+        slidesPerGroup: 27,
+        allowTouchMove: false,
+      },
+    },
+    on: {
+      init: () => {
+        if (window.innerWidth > 992) {
+          // console.log(1111)
+          // this.slideTo(this.slides.length-1,0)
+          // this.dateSwiper!.initialSlide(10)
+        }
+      },
+      navigationNext: function () {
+        console.log('navigation next');
+      }
+    }
+  };
 
   constructor() {
-    for (let i = 0; i < 100; i++) {
-      this.dates.push('1');
+    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август",
+      "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+    const days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+    let today = new Date();
+    today.setDate(today.getDate() - this.daysOffset);
+    for (let i = 0; i < this.daysInDateFeed; i++) {
+      let className = ''
+      const day = today.getDay();
+      const dateDay = today.getDate()
+      className += (day === 0 || day === 6 ? '_weekend' : '')
+      className += (dateDay === 1 || i === 0 ? ' _first-day' : '')
+      this.dates.push({
+        date: dateDay.toString(),
+        month: months[today.getMonth()],
+        weekDay: days[day],
+        class: className
+      });
+      today.setDate(today.getDate() + 1);
     }
   }
 
   ngAfterViewInit() {
-    console.log('date-feed');
-    const dateSwiper = new Swiper('.swiper111', {
-      slidesPerView: 26,
-      slidesPerGroup: 26,
-      spaceBetween: 0,
-      allowTouchMove: false,
-      navigation: true,
-      // navigation: {
-      //   nextEl: '.date-feed .swiper-button-next',
-      //   prevEl: '.date-feed .swiper-button-prev',
-      // },
-      breakpoints: {
-        100: {
-          slidesPerView: "auto",
-          slidesPerGroup: 1,
-          allowTouchMove: true,
-        },
-        992: {
-          slidesPerView: 27,
-          slidesPerGroup: 27,
-          allowTouchMove: false,
-        },
-      },
-      on: {
-        init: function () {
-          if(window.innerWidth>992){
-            console.log(1111)
-            // this.slideTo(this.slides.length-1,0)
-          }
-        },
-        navigationNext: function () {
-          console.log('navigation next');
-        }
-      },
-    });
-    console.log(dateSwiper);
-    console.log(dateSwiper.navigation);
-    console.log(document.querySelector('.swiper-button-next'))
+    this.dateSwiper = document.querySelector('.date-swiper');
+    if (this.dateSwiper) {
+      Object.assign(this.dateSwiper, this.dateSwiperParams);
+      this.dateSwiper.initialize();
+      // this.dateSwiper.swiper.slideTo(60);
+    }
   }
-
 }
 
 
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {PosterInfoComponent} from '../../shared/components/events/poster-info/poster-info.component';
 import {DateFeedComponent} from '../../shared/components/page-blocks/date-feed/date-feed.component';
 import {NgIf} from '@angular/common';
+import {DateFilterComponent} from '../../shared/components/page-blocks/date-filter/date-filter.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [PosterInfoComponent, DateFeedComponent, NgIf],
+  imports: [PosterInfoComponent, DateFeedComponent, NgIf, DateFilterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
