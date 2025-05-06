833a79f9da3d0308e0e2fe2cf37c27c2c19346fd
kazacoder <avtk.tlt@gmail.com>
2025-05-06 10:31:38 +0300

    page-under-construction.component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import {EventsListComponent} from './views/event/events-list/events-list.compone
 import {BlogComponent} from './views/article/blog/blog.component';
 import {MasterDetailComponent} from './views/master/master-detail/master-detail.component';
 import {PageNotFoundComponent} from './shared/components/page-not-found/page-not-found.component';
+import {
+  PageUnderConstructionComponent
+} from './shared/components/page-under-construction/page-under-construction.component';
 
 export const routes: Routes = [
   {
@@ -17,7 +20,9 @@ export const routes: Routes = [
       {path: 'master', component: MasterDetailComponent, title: 'Мастер',},
       {path: 'events', component: EventsListComponent, title: 'Мероприятия',},
       {path: 'articles', component: BlogComponent, title: 'Статьи',},
-      {path: '**', component: PageNotFoundComponent, title: 'Страница не найдена'},
+      {path: 'favorite', component: PageUnderConstructionComponent, title: 'Избранное'},
+      {path: 'notification', component: PageUnderConstructionComponent, title: 'Уведомления'},
+      {path: '**', component: PageNotFoundComponent, title: '404'},
     ]
   },
 ];
---
+++ src/app/shared/components/page-under-construction/page-under-construction.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-page-under-construction',
+  imports: [
+    RouterLink
+  ],
+  standalone: true,
+  templateUrl: './page-under-construction.component.html',
+  styleUrl: './page-under-construction.component.scss'
+})
+export class PageUnderConstructionComponent {
+
+}
