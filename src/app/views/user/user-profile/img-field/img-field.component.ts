c3fbae888e2042507d1671f1cfa40f0d8c6fe265
user <user@test.net>
2025-05-15 21:58:12 +0300

    Showing user photo realized.

--- src/app/views/user/user-profile/img-field/img-field.component.ts
+++ src/app/views/user/user-profile/img-field/img-field.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgOptimizedImage, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'img-field',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgStyle
+  ],
   standalone: true,
   templateUrl: './img-field.component.html',
   styleUrl: './img-field.component.scss'
 })
-export class ImgFieldComponent {
+export class ImgFieldComponent implements OnInit {
+  @Input()
+  userPhotoUrl: string | null  = null;
 
+  userPhoto: string = 'assets/img/img.svg'
+
+  ngOnInit() {
+    if (this.userPhotoUrl) {
+      this.userPhoto = this.userPhotoUrl
+    }
+  }
 }
