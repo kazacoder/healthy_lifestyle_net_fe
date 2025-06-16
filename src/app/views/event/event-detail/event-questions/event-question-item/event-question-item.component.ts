6f3c754547d8964d30d5d2249891e1eceac5ec4e
kazacoder <avtk.tlt@gmail.com>
2025-06-16 13:27:59 +0300

    Event answer validation realized.

--- src/app/views/event/event-detail/event-questions/event-question-item/event-question-item.component.ts
+++ src/app/views/event/event-detail/event-questions/event-question-item/event-question-item.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {QuestionExtendedType} from '../../../../../../types/question-extended.type';
-import {DatePipe, NgIf} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DatePipe, NgClass, NgIf} from '@angular/common';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {DefaultResponseType} from '../../../../../../types/default-response.type';
 import {HttpErrorResponse} from '@angular/common/http';
 import {Subscription} from 'rxjs';
@@ -17,7 +17,8 @@ import {Settings} from '../../../../../../settings/settings';
     NgIf,
     DatePipe,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgClass
   ],
   standalone: true,
   templateUrl: './event-question-item.component.html',
@@ -34,68 +35,93 @@ export class EventQuestionItemComponent implements OnInit, OnDestroy {
   @Input() name: string = '';
   @Input() question: QuestionExtendedType | null = null;
   @Input() eventAuthor: number | undefined | null = null;
-  @Input() answer: string = '';
+  answerSaved: string = '';
+  hasChanged: boolean = false;
+  answerForm = this.fb.group({
+    answer: ['', [Validators.required, Validators.maxLength(Settings.maxAnswerLength),
+      Validators.minLength(Settings.minAnswerLength)]],
+  })
 
   constructor(private eventService: EventService,
               private _snackBar: MatSnackBar,
-              private userService: UserService,) {
+              private userService: UserService,
+              private fb: FormBuilder) {
   }
 
   ngOnInit() {
     this.userId = this.userService.getUserId()
-    this.answer = this.question?.answer ? this.question?.answer.text : ''
+    this.answerForm.get('answer')?.setValue(this.question?.answer ? this.question?.answer.text : '');
+    this.answerSaved = this.answerForm.value.answer as string;
   }
 
-  allowEditAnswer (flag: boolean) {
+  allowEditAnswer(flag: boolean) {
     this.editAnswer = flag;
   }
 
+  onAnswerChange(): void {
+    this.hasChanged = !(this.answerForm.value.answer === this.answerSaved);
+  }
+
   proceed() {
-    if (this.question?.answer?.id) {
-      this.updateAnswerSubscription = this.eventService.updateAnswer(this.question?.answer.id, this.answer).subscribe({
-        next: (data: AnswerResponseType | DefaultResponseType) => {
-          if ((data as DefaultResponseType).detail !== undefined) {
-            const error = (data as DefaultResponseType).detail;
-            this._snackBar.open(error);
-            throw new Error(error);
-          }
-          this._snackBar.open('Ваш ответ успешно сохранен');
-          this.getQuestion();
-          this.editAnswer = false;
-        },
-        error: (errorResponse: HttpErrorResponse) => {
-          if (errorResponse.error && errorResponse.error.detail) {
-            this._snackBar.open(errorResponse.error.detail)
-          } else {
-            this._snackBar.open('Ошибка сохранения ответа')
-          }
-        }
-      });
-    } else {
-      if (this.question?.id) {
-        this.createAnswerSubscription = this.eventService.createAnswer(this.question.id, this.answer).subscribe({
+    if (this.hasChanged && this.answerForm.valid) {
+      if (this.question?.answer?.id && this.answerForm.value.answer) {
+        this.updateAnswerSubscription = this.eventService.updateAnswer(this.question?.answer.id, this.answerForm.value.answer).subscribe({
           next: (data: AnswerResponseType | DefaultResponseType) => {
             if ((data as DefaultResponseType).detail !== undefined) {
               const error = (data as DefaultResponseType).detail;
               this._snackBar.open(error);
               throw new Error(error);
             }
-            this._snackBar.open('Ваш ответ успешно отправлен');
+            this._snackBar.open('Ваш ответ успешно сохранен');
             this.getQuestion();
+            this.editAnswer = false;
+            this.hasChanged = false;
+            this.answerForm.markAsPristine();
+            this.answerForm.markAsUntouched();
+            this.answerSaved = this.answerForm.value.answer as string;
           },
           error: (errorResponse: HttpErrorResponse) => {
             if (errorResponse.error && errorResponse.error.detail) {
               this._snackBar.open(errorResponse.error.detail)
             } else {
-              this._snackBar.open('Ошибка отправки ответа')
+              this._snackBar.open('Ошибка сохранения ответа')
             }
           }
         });
+      } else {
+        if (this.question?.id && this.answerForm.value.answer) {
+          this.createAnswerSubscription = this.eventService.createAnswer(this.question.id, this.answerForm.value.answer).subscribe({
+            next: (data: AnswerResponseType | DefaultResponseType) => {
+              if ((data as DefaultResponseType).detail !== undefined) {
+                const error = (data as DefaultResponseType).detail;
+                this._snackBar.open(error);
+                throw new Error(error);
+              }
+              this._snackBar.open('Ваш ответ успешно отправлен');
+              this.getQuestion();
+              this.hasChanged = false;
+              this.answerSaved = this.answerForm.value.answer as string;
+              this.answerForm.markAsPristine();
+              this.answerForm.markAsUntouched();
+            },
+            error: (errorResponse: HttpErrorResponse) => {
+              if (errorResponse.error && errorResponse.error.detail) {
+                this._snackBar.open(errorResponse.error.detail)
+              } else {
+                this._snackBar.open('Ошибка отправки ответа')
+              }
+            }
+          });
+        }
       }
+    } else if (this.answerForm.invalid) {
+      this._snackBar.open('Форма заполнена некорректно')
+    } else {
+      this._snackBar.open('Вы не изменили текст ответа!')
     }
   }
 
-  getQuestion () {
+  getQuestion() {
     this.getQuestionSubscription = this.eventService.getQuestion(this.question!.id.toString()).subscribe({
       next: (question: QuestionExtendedType | DefaultResponseType) => {
         if ((question as DefaultResponseType).detail !== undefined) {
@@ -115,12 +141,14 @@ export class EventQuestionItemComponent implements OnInit, OnDestroy {
     });
   }
 
-  cancelAnswer () {
+  cancelAnswer() {
     if (this.editAnswer) {
       this.editAnswer = !this.editAnswer
-    } else {
-      this.answer = ''
     }
+    this.answerForm.get('answer')?.setValue(this.question?.answer ? this.question.answer.text : '');
+    this.answerForm.markAsUntouched();
+    this.answerForm.markAsPristine();
+
   }
 
   ngOnDestroy() {
