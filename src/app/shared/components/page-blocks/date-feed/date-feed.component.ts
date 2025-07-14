02593b56ab209e568b603d0ba643596884e0743a
kazacoder <avtk.tlt@gmail.com>
2025-07-14 17:35:40 +0300

    Date feed component configured.

--- src/app/shared/components/page-blocks/date-feed/date-feed.component.ts
+++ src/app/shared/components/page-blocks/date-feed/date-feed.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnDestroy, OnInit} from '@angular/core';
 import {DateItemComponent} from '../../cards/date-item/date-item.component';
 import {SwiperContainer} from 'swiper/element/bundle';
 // import 'swiper/css/navigation'
@@ -16,12 +16,13 @@ import {ActivatedRoute, Router} from '@angular/router';
   templateUrl: './date-feed.component.html',
   styleUrl: './date-feed.component.scss'
 })
-export class DateFeedComponent implements OnInit, AfterViewInit {
+export class DateFeedComponent implements OnInit, AfterViewInit, OnDestroy {
   daysOffset = 30;
   daysInDateFeed = 100;
   activatedRouterSubscription: Subscription | null = null;
   dates: { date: string | null, day: string, month: string, class: string, weekDay: string }[] = [];
   dateSwiper: SwiperContainer | null = null;
+  period: (string | null)[] = []
   dateSwiperParams = {
     slidesPerView: 26,
     slidesPerGroup: 26,
@@ -57,8 +58,9 @@ export class DateFeedComponent implements OnInit, AfterViewInit {
       },
       click: () => {
         const idx = this.dateSwiper?.swiper.clickedIndex;
-        if (idx) {
+        if (idx || idx === 0) {
           if (this.dates.filter(item => item.class.includes('_selected')).length > 1) {
+            this.period = []
             this.dates.map(item => {
               if (item.class.includes('_weekend')) {
                 item.class = '_weekend'
@@ -67,10 +69,37 @@ export class DateFeedComponent implements OnInit, AfterViewInit {
               }
             })
           }
+          let dateTo = null
+          if (this.period[0] !== this.dates[idx].date) {
+            this.period.push(this.dates[idx].date)
+          } else {
+            dateTo = this.dates[idx].date
+          }
           this.dates[idx].class = '_selected';
-          console.log(this.dates[idx].date);
+          const parsedDates = this.period.map((date) => new Date(date as string).getTime());
+          const minDate = new Date(Math.min(...parsedDates));
+          const maxDate = new Date(Math.max(...parsedDates));
+          const dateFrom = minDate.toISOString().slice(0, 10)
+
+          if (this.period.length > 1) {
+            dateTo = maxDate.toISOString().slice(0, 10)
+            this.period = []
+          }
+          const queryParams = {
+            ...this.activatedRoute.snapshot.queryParams, ['date_from']: dateFrom, ['date_to']: dateTo
+          };
+          if (dateTo === dateFrom) {
+            this.period = []
+          }
+          console.log(this.period)
+
+          this.router.navigate([], {
+            relativeTo: this.activatedRoute,
+            queryParams,
+            queryParamsHandling: 'merge'
+          }).then();
         }
-      }
+      },
     }
   };
 
@@ -103,8 +132,6 @@ export class DateFeedComponent implements OnInit, AfterViewInit {
     this.activatedRouterSubscription = this.activatedRoute.queryParams.subscribe(params => {
       const dateForm = params['date_from']
       const dateTo = params['date_to']
-      console.log(new Date(dateForm))
-      console.log(this.dates[35].date)
       const dateFromIndex = this.dates.findIndex(item => item.date === dateForm)
       const dateToIndex = this.dates.findIndex(item => item.date === dateTo)
 
@@ -116,11 +143,19 @@ export class DateFeedComponent implements OnInit, AfterViewInit {
         }
       })
 
+      console.log(123)
       if (dateFromIndex !== -1) {
         this.dates[dateFromIndex].class = '_selected';
         if (dateTo) {
           this.dates[dateToIndex].class = '_selected';
+          if (dateTo === dateForm) {
+            this.dates[dateFromIndex].class = '_selected exact';
+          }
         }
+      }
+
+      if (!dateForm) {
+        this.period = [];
       }
     });
   }
@@ -132,6 +167,10 @@ export class DateFeedComponent implements OnInit, AfterViewInit {
       this.dateSwiper.initialize();
       // this.dateSwiper.swiper.slideTo(60);
     }
+  }
+
+  ngOnDestroy() {
+    this.activatedRouterSubscription?.unsubscribe();
   }
 }
 
