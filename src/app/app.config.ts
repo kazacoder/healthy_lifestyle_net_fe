a01f225cb6eaf1756f2d6ee4612b6d8e306b562a
kazacoder <avtk.tlt@gmail.com>
2025-08-29 15:13:32 +0300

    cCredential interceptor added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,7 @@ import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@ang
 import {MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material/snack-bar';
 import {AuthInterceptor} from './core/auth/auth.interceptor';
 import {MAT_DATE_LOCALE} from '@angular/material/core';
+import {CredentialsInterceptor} from './core/credentials/credentials.interceptor';
 
 
 const scrollConfig: InMemoryScrollingOptions = {
@@ -29,6 +30,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500,}},
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: CredentialsInterceptor, multi: true},
     {provide: MAT_DATE_LOCALE, useValue: 'ru-RU'}
   ]
 };
